e234eb16b8dbca1e6e384cbc9d2f86bcaca3362b
michael fahrner <Mike Fahrner>
2016-09-23 14:08:20 -0400

    adding postgres dependency and procfile

--- src/main/java/com/theironyard/charlotte/controllers/WebsiteController.java
+++ src/main/java/com/theironyard/charlotte/controllers/WebsiteController.java
@@ -1,7 +1,18 @@
 package com.theironyard.charlotte.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by mfahrner on 9/23/16.
  */
+@Controller
 public class WebsiteController {
+
+    @RequestMapping (path = "/", method = RequestMethod.GET)
+    public String home(Model model) {
+        return "home";
+    }
 }
